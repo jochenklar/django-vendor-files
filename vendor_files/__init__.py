a73d22e22d7b01cfdb91fa9317efc01c231362d3
Jochen Klar <mail@jochenklar.de>
2022-01-11 13:07:14 +0100

    Update version

--- vendor_files/__init__.py
+++ vendor_files/__init__.py
@@ -1,8 +1,8 @@
 __title__ = 'django-vendor-files'
-__version__ = '0.2'
+__version__ = '0.3'
 __author__ = 'Jochen Klar'
-__email__ = 'jklar@aip.de'
+__email__ = 'mail@jochenklar.de'
 __license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2016-2017 Leibniz Institute for Astrophysics Potsdam (AIP)'
+__copyright__ = 'Copyright 2016-2022 Leibniz Institute for Astrophysics Potsdam (AIP)'
 
 VERSION = __version__
